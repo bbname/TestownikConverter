6a11fbe1554529fc71bc040c12b46b05f3f67ee0
bbname <bartlomiej.bienczyk@gmail.com>
2018-01-28 22:58:27 +0100

    I've added basic architecture.

---
+++ TestownikConverter.Reader/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestownikConverter.Reader
+{
+    public class Class1
+    {
+    }
+}
